64f3467dfd05b7c9bc254ebb3975aa9b4f1c17ad
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-20 08:37:51 +0100

    Add personal information section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import Section from "./components/Section";
+
 function App() {
   return (
     <div>
+      <Section title="Personal Information" fields={
+        [
+          {label: 'Firstname', type: 'text'},
+          {label: 'Lastname', type: 'text'},
+          {label: 'Telephone', type: 'text'},
+          {label: 'Email', type: 'email'},
+        ]
+      }/>
     </div>
   );
 }
