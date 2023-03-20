9b93c749810230bc1fc455466a18e5a2cc558d29
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-20 08:34:34 +0100

    Add fields to section, add edit button to section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import Field from './Field';
 
 class Section extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editEnabled: false};
+        this.onEditButtonClicked = this.onEditButtonClicked.bind(this);
+    }
+
+    onEditButtonClicked() {
+        this.setState({editEnabled: !this.state.editEnabled});
+    }
+
     render() {
-        return (
-        <div>
-            <h2>{this.props.title}</h2>
-        </div>
-        )
+        const fields = this.props.fields;
+            return (
+                <div>
+                    <h2>{this.props.title}</h2>
+                    {fields && 
+                    (
+                        <div>
+                            {fields.map((field) => {
+                            return <Field type={field.type} label={field.label}/>
+                            })}
+                            <button
+                                onClick={this.onEditButtonClicked}>
+                                {this.state.editEnabled ? 'Save' : 'Edit'}
+                            </button>
+                        </div>
+                    )}
+                </div>
+            )
     }
 }
 
