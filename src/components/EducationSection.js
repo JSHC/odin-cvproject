08bf0d946bbafc0e367d83969afdb7f986c1b5a9
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-21 06:45:14 +0100

    Add educations array and rendering, add setState call to add new educations

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import EducationComponent from './EducationComponent';
 import Section from './Section';
+import uniqid from 'uniqid';
 
 class EducationSection extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {editEnabled: true}
+        this.state = {
+            editEnabled: true,
+            educations: [],
+        }
         this.onEditEnabledChanged = this.onEditEnabledChanged.bind(this);
         this.onAddButtonClicked = this.onAddButtonClicked.bind(this);
     }
@@ -17,7 +21,11 @@ class EducationSection extends React.Component {
 
     onAddButtonClicked() {
         console.log('add clicked');
+        this.setState({
+            educations: this.state.educations.concat(<EducationComponent editEnabled={this.state.editEnabled} key={uniqid()}/>)
+        })
     }
+
 
     render() {
         const { editEnabled } = this.state;
@@ -28,6 +36,7 @@ class EducationSection extends React.Component {
                 onEditEnabledChanged={this.onEditEnabledChanged}
             >
                 <EducationComponent editEnabled={editEnabled} />
+                {this.state.educations}
                 <button onClick={this.onAddButtonClicked} >Add</button>
             </Section>
         )
