8900661a808d4dfe04c70ba637eaf30d52f94a4e
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-21 13:18:32 +0100

    Add Section to practical experience section, add edit enabled state

--- src/components/PracticalExperienceSection.js
+++ src/components/PracticalExperienceSection.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Section from './Section';
 
 class PracticalExperienceSection {
+    constructor(props) {
+        super(props);
+        this.state = {editEnabled: true}
+
+        this.onEditEnabledChanged = this.onEditEnabledChanged.bind(this);
+    }
+
+    onEditEnabledChanged() {
+        this.setState({editEnabled: !this.state.editEnabled})
+    }
+
     render() {
+        const { editEnabled } = this.state;
         return (
-            <div></div>
+            <Section
+                title="Practical Experience"
+                editEnabled={editEnabled}
+                onEditEnabledChanged={this.onEditEnabledChanged}
+            >
+                
+            </Section>
         )
     }
 }
