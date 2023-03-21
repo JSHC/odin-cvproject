bce53dfcda2dea8537168ae2565260d638535744
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-21 13:13:02 +0100

    Create PracticalExperienceSection

---
+++ src/components/PracticalExperienceSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PracticalExperienceSection {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default PracticalExperienceSection;
