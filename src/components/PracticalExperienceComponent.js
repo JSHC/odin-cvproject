4ac6ad3f5b21ec63b28848c480c39fb2fc90f21a
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-21 20:59:48 +0100

    Add editEnabled prop to PracticalExperienceComponent, pass prop down to fields

--- src/components/PracticalExperienceComponent.js
+++ src/components/PracticalExperienceComponent.js
@@ -4,27 +4,33 @@ import Field from './Field';
 class PracticalExperienceComponent extends React.Component {
 
     render() {
+        const { editEnabled } = this.props;
         return (
             <div>
                 <Field 
                     label="Company Name"
                     type="text"
+                    editEnabled={editEnabled}
                 />
                 <Field 
                     label="Position Title"
                     type="text"
+                    editEnabled={editEnabled}
                 />
                 <Field 
                     label="Description"
                     type="text"
+                    editEnabled={editEnabled}
                 />
                 <Field
                     label="Start Date"
                     type="date"
+                    editEnabled={editEnabled}
                 />
                 <Field
                     label="End Date"
                     type="date"
+                    editEnabled={editEnabled}
                 />
             </div>
         )
