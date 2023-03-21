e5d60b6bf2be505cb9ac69137a86b484d94a2981
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-21 06:19:15 +0100

    Add fields to education component

--- src/components/EducationComponent.js
+++ src/components/EducationComponent.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Field from './Field';
 
 class EducationComponent extends React.Component {
     render() {
+        const { editEnabled } = this.props;
         return (
-            <div></div>
+            <div>
+                <Field 
+                    label="School Name"
+                    type="text" 
+                    editEnabled={editEnabled} />
+                <Field 
+                    label="Education"
+                    type="text"
+                    editEnabled={editEnabled} />
+                <Field
+                    label="Start year"
+                    type="number"
+                    editEnabled={editEnabled} />
+                <Field
+                    label="End year"
+                    type="number"
+                    editEnabled={editEnabled} />
+            </div>
         )
     }
 }
