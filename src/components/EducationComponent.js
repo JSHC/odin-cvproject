e4b25cea4d62a0a4f7a5e30ffdd355cf3fa39b02
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-22 06:47:28 +0100

    Add css classes to EducationComponent

--- src/components/EducationComponent.js
+++ src/components/EducationComponent.js
@@ -10,18 +10,22 @@ class EducationComponent extends React.Component {
                 <Field 
                     label="School Name"
                     type="text" 
+                    className="school-name"
                     editEnabled={editEnabled} />
                 <Field 
                     label="Education"
                     type="text"
+                    className="education"
                     editEnabled={editEnabled} />
                 <Field
                     label="Start year"
                     type="number"
+                    className="start-year"
                     editEnabled={editEnabled} />
                 <Field
                     label="End year"
                     type="number"
+                    className="end-year"
                     editEnabled={editEnabled} />
                 {this.props.canRemove && 
                 <button onClick={(e) => this.props.onRemoveButtonClicked(e, this.props.id)}>Remove</button>
