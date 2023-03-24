12ced424703e3d6f5b1155292e2523b46da66f10
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-24 08:35:22 +0100

    Rename editEnabled to previewEnabled

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -4,11 +4,11 @@ import '../styles/ToggleButton.css';
 class ToggleButton extends React.Component {
 
     render() {
-        const { editEnabled, onToggleChanged } = this.props;
+        const { previewEnabled, onToggleChanged } = this.props;
         return (
             <div 
                 onClick={onToggleChanged}
-                className={`toggle-button ${editEnabled ? 'enabled' : 'disabled'}`}
+                className={`toggle-button ${previewEnabled ? 'enabled' : 'disabled'}`}
             >
                 <div className='toggle-button-inner'>
                     <div className='toggle-button-circle'></div>
