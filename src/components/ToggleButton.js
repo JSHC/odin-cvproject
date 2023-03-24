4284983a4cccedd404c240159c7614f637155eb2
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-24 08:19:35 +0100

    Add divs and classNames

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -3,8 +3,16 @@ import React from 'react';
 class ToggleButton extends React.Component {
 
     render() {
+        const { editEnabled, onToggleChanged } = this.props;
         return (
-            <div></div>
+            <div 
+                onClick={onToggleChanged}
+                className={`toggle-button ${editEnabled ? 'enabled' : 'disabled'}`}
+            >
+                <div className='toggle-button-inner'>
+                    <div className='toggle-button-circle'></div>
+                </div>
+            </div>
         )
     }
 }
