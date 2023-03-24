d381b9060926d2c6468d06a327a5da99eca46425
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-24 08:15:14 +0100

    Create toggle button class

---
+++ src/components/ToggleButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ToggleButton extends React.Component {
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default ToggleButton;
