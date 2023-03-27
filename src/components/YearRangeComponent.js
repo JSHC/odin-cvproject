26fb6d76ec315d7cb9204a12782bda84a3debb86
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-27 12:55:02 +0200

    Add fields

--- src/components/YearRangeComponent.js
+++ src/components/YearRangeComponent.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Field from './Field';
 
 class YearRangeComponent extends React.Component {
     render() {
+        const { editEnabled } = this.props;
         return (
-            <div></div>
+            <div className='year-range-component'>
+                <Field 
+                    label="Start Year"
+                    type="number"
+                    size={5}
+                    editEnabled={editEnabled}
+                    className="start-year"
+                />
+                <span>-</span>
+                <Field 
+                    label="End Year"
+                    type="number"
+                    size={5}
+                    editEnabled={editEnabled}
+                    className="end-year"
+                />
+            </div>
         )
     }
 }
