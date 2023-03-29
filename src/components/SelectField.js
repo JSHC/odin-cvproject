6fbdcf97f8c79f90d855228d808eca7ad8a3ed1c
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-29 08:25:03 +0200

    Create SelectField

---
+++ src/components/SelectField.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import React from 'react';
+
+class SelectField extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onFieldChange = this.onFieldChange.bind(this);
+        
+        const year = new Date().getFullYear();
+        this.years = _.range(1900, year);
+    }
+    onFieldChange(e) {
+        this.props.onFieldValueChanged(e);
+    }
+
+    render() {
+        return (
+            <div className={this.props.className + '-container'}>
+            {this.props.editEnabled ? 
+            (
+                <label className={this.props.className + '-label'}>
+                    {this.props.label}
+                    
+                            <select name={this.props.name} onChange={this.props.onFieldValueChanged}>
+                                {this.years.map(year => {
+                                    return <option name={this.props.name} selected={this.props.fieldValue === year}>{year}</option>
+                                })}
+                            </select>
+                </label>
+            )
+            :
+                <div className={this.props.className}>
+                    {this.props.fieldValue}
+                </div>
+            }
+            </div>
+        )
+    }
+}
+
+export default SelectField;
