771ebb3ef4dec5b776989cf7658d792c576e4e5a
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-29 20:52:27 +0200

    Add onClick prop

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 class IconButton extends React.Component {
     render() {
         return (
-            <button className='icon-button'>
+            <button className='icon-button' onClick={this.props.onClick}>
                 {this.props.icon}
                 <span>{this.props.text}</span>
             </button>
