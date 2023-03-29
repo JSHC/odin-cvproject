8fd9fa37c8c513495123a8ce4565bb0c03565c5f
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-29 20:43:07 +0200

    Create IconButton class

---
+++ src/components/IconButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class IconButton extends React.Component {
+    render() {
+        return (
+            <button></button>
+        )
+    }
+}
+
+export default IconButton;
