73edfcc4a68621e15e4b7677751799df2b37a2d5
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-31 15:31:40 +0200

    Refactor PracticalExperienceComponent to functional component

--- src/components/PracticalExperienceSection.js
+++ src/components/PracticalExperienceSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PracticalExperienceComponent from './PracticalExperienceComponent';
 import Section from './Section';
 import uniqid from 'uniqid';
@@ -6,74 +6,62 @@ import '../styles/PracticalExperienceSection.css';
 import { FaPlusCircle } from 'react-icons/fa';
 import IconButton from './IconButton';
 
-class PracticalExperienceSection extends React.Component{
-    constructor(props) {
-        super(props);
+const PracticalExperienceSection = (props) => {
+    const [editEnabled, setEditEnabled] = useState(true);
+    const [practicalExperiences, setPracticalExperiences] = useState(
+        [{id: uniqid(), canRemove: false}]
+    );
 
-        this.state = {
-            editEnabled: true,
-            practicalExperiences: [
-                {id: uniqid(), canRemove: false}
-            ]
-        }
-
-        this.onEditEnabledChanged = this.onEditEnabledChanged.bind(this);
-        this.onRemoveButtonClicked = this.onRemoveButtonClicked.bind(this);
-        this.onAddButtonClicked = this.onAddButtonClicked.bind(this);
+    const onEditEnabledChanged = () => {
+        setEditEnabled(!editEnabled);
     }
 
-    onEditEnabledChanged() {
-        this.setState({editEnabled: !this.state.editEnabled})
-    }
-
-    onRemoveButtonClicked(e, id) {
-        const newPracticalExperiences = this.state.practicalExperiences
+    const onRemoveButtonClicked = (e, id) => {
+        const newPracticalExperiences = practicalExperiences
         .filter((item) => {
             return id !== item.id;
         })
-        this.setState({practicalExperiences: newPracticalExperiences});
+        setPracticalExperiences(newPracticalExperiences);
     }
 
-    onAddButtonClicked() {
-        this.setState((prevState) => (
-            {practicalExperiences: prevState.practicalExperiences.concat(
+    const onAddButtonClicked = () => {
+        setPracticalExperiences(
+            practicalExperiences.concat(
                 {id: uniqid(), canRemove: true}
-            )}
-        ))
+            )
+        );
     }
 
-    render() {
-        const editEnabled = this.state.editEnabled && !this.props.previewEnabled;
-        return (
-            <Section
-                title="Practical Experience"
-                editEnabled={editEnabled}
-                onEditEnabledChanged={this.onEditEnabledChanged}
-                className="practical-epxerience-section"
-                previewEnabled={this.props.previewEnabled}
-            >
-                {this.state.practicalExperiences.map((item) => {
-                    return <PracticalExperienceComponent 
-                        id={item.id}
-                        editEnabled={editEnabled}
-                        onRemoveButtonClicked={this.onRemoveButtonClicked}
-                        key={item.id}
-                        canRemove={item.canRemove}
-                        className="practical-experience-component"
-                    />
-                })}
+    const canEdit = editEnabled && !props.previewEnabled;
+    return (
+        <Section
+            title="Practical Experience"
+            editEnabled={canEdit}
+            onEditEnabledChanged={onEditEnabledChanged}
+            className="practical-epxerience-section"
+            previewEnabled={props.previewEnabled}
+        >
+            {practicalExperiences.map((item) => {
+                return <PracticalExperienceComponent 
+                    id={item.id}
+                    editEnabled={canEdit}
+                    onRemoveButtonClicked={onRemoveButtonClicked}
+                    key={item.id}
+                    canRemove={item.canRemove}
+                    className="practical-experience-component"
+                />
+            })}
 
-                {this.props.previewEnabled === false && 
-                    <IconButton
-                        className='add-button icon-button-green'
-                        onClick={this.onAddButtonClicked}
-                        text='Add'
-                        icon={<FaPlusCircle />}
-                    />
-                }
-            </Section>
-        )
-    }
+            {props.previewEnabled === false && 
+                <IconButton
+                    className='add-button icon-button-green'
+                    onClick={onAddButtonClicked}
+                    text='Add'
+                    icon={<FaPlusCircle />}
+                />
+            }
+        </Section>
+    )
 }
 
 export default PracticalExperienceSection;
