48c50cd6712ce8b559f3373d9de6fafa4367fa22
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-31 14:53:24 +0200

    Refactor ToggleButton to functional component

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import '../styles/ToggleButton.css';
 
-class ToggleButton extends React.Component {
-
-    render() {
-        const { previewEnabled, onToggleChanged } = this.props;
-        return (
-            <div 
-                onClick={onToggleChanged}
-                className={`toggle-button ${previewEnabled ? 'enabled' : 'disabled'}`}
-            >
-                <div className='toggle-button-inner'>
-                    <div className='toggle-button-circle'></div>
-                </div>
+const ToggleButton = (props) => {
+    const { previewEnabled, onToggleChanged } = props;
+    return (
+        <div 
+            onClick={onToggleChanged}
+            className={`toggle-button ${previewEnabled ? 'enabled' : 'disabled'}`}
+        >
+            <div className='toggle-button-inner'>
+                <div className='toggle-button-circle'></div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ToggleButton;
