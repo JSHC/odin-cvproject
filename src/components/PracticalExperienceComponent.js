5842e25fed69d1821fcbdcf94293227f1d068ad7
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-03 10:09:48 +0200

    Refactor PracticalExperienceComponent to functional component

--- src/components/PracticalExperienceComponent.js
+++ src/components/PracticalExperienceComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Field from './Field';
 import '../styles/PracticalExperienceComponent.css';
 import YearRangeComponent from './YearRangeComponent';
@@ -6,78 +6,65 @@ import { FaTimesCircle } from 'react-icons/fa';
 import IconButton from './IconButton';
 import TextAreaField from './TextAreaField';
 
-class PracticalExperienceComponent extends React.Component {
+const PracticalExperienceComponent = (props) => {
+    const [positionTitle, setPositionTitle] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [description, setDescription] = useState('');
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            positionTitle: '',
-            companyName: '',
-            description: '',
-        }
-
-        this.onPositionTitleChanged = this.onPositionTitleChanged.bind(this);
-        this.onCompanyNameChanged = this.onCompanyNameChanged.bind(this);
-        this.onDescriptionChanged = this.onDescriptionChanged.bind(this);
+    const onPositionTitleChanged = (e) => {
+        setPositionTitle(e.target.value)
     }
 
-    onPositionTitleChanged(e) {
-        this.setState({positionTitle: e.target.value})
+    const onCompanyNameChanged = (e) => {
+        setCompanyName(e.target.value)
     }
 
-    onCompanyNameChanged(e) {
-        this.setState({companyName: e.target.value})
+    const onDescriptionChanged = (e) => {
+        setDescription(e.target.value)
     }
 
-    onDescriptionChanged(e) {
-        this.setState({description: e.target.value})
-    }
-
-    render() {
-        const { editEnabled } = this.props;
-        return (
-            <div className={this.props.className}>
-                <Field 
-                    label="Position Title"
-                    type="text"
-                    editEnabled={editEnabled}
-                    className="position-title"
-                    size={25}
-                    fieldValue={this.state.positionTitle}
-                    onFieldValueChanged={this.onPositionTitleChanged}
+    const { editEnabled } = props;
+    return (
+        <div className={props.className}>
+            <Field 
+                label="Position Title"
+                type="text"
+                editEnabled={editEnabled}
+                className="position-title"
+                size={25}
+                fieldValue={positionTitle}
+                onFieldValueChanged={onPositionTitleChanged}
+            />
+            <Field 
+                label="Company Name"
+                type="text"
+                editEnabled={editEnabled}
+                className="company-name"
+                size={35}
+                fieldValue={companyName}
+                onFieldValueChanged={onCompanyNameChanged}
+            />
+            <TextAreaField 
+                label="Description"
+                editEnabled={editEnabled}
+                className="description"
+                fieldValue={description}
+                onFieldValueChanged={onDescriptionChanged}
+            />
+            <YearRangeComponent 
+                editEnabled={editEnabled}
+                className='practical-experience-years'
+            />
+            {props.canRemove && 
+                <IconButton
+                    className='remove-practical-experience-button icon-button-red'
+                    onClick={(e) => props.onRemoveButtonClicked(e, props.id)}
+                    text='Remove'
+                    icon={<FaTimesCircle />}
                 />
-                <Field 
-                    label="Company Name"
-                    type="text"
-                    editEnabled={editEnabled}
-                    className="company-name"
-                    size={35}
-                    fieldValue={this.state.companyName}
-                    onFieldValueChanged={this.onCompanyNameChanged}
-                />
-                <TextAreaField 
-                    label="Description"
-                    editEnabled={editEnabled}
-                    className="description"
-                    fieldValue={this.state.description}
-                    onFieldValueChanged={this.onDescriptionChanged}
-                />
-                <YearRangeComponent 
-                    editEnabled={editEnabled}
-                    className='practical-experience-years'
-                />
-                {this.props.canRemove && 
-                    <IconButton
-                        className='remove-practical-experience-button icon-button-red'
-                        onClick={(e) => this.props.onRemoveButtonClicked(e, this.props.id)}
-                        text='Remove'
-                        icon={<FaTimesCircle />}
-                    />
-                }
-            </div>
-        )
-    }
+            }
+        </div>
+    )
 }
 
 export default PracticalExperienceComponent;
