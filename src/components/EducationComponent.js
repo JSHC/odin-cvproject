c550f439f60ba6fed5b2889f03642f1b763c9654
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-03 10:24:18 +0200

    Refactor EducationComponent to functional component

--- src/components/EducationComponent.js
+++ src/components/EducationComponent.js
@@ -1,69 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Field from './Field';
 import '../styles/EducationComponent.css';
 import YearRangeComponent from './YearRangeComponent';
 import { FaTimesCircle } from 'react-icons/fa';
 import IconButton from './IconButton';
 
-class EducationComponent extends React.Component {
-    constructor(props) {
-        super(props);
+const EducationComponent = (props) => {
+    const [education, setEducation] = useState('');
+    const [schoolName, setSchoolName] = useState('');
 
-        this.state = {
-            education: '',
-            schoolName: '',
-        }
-
-        this.onEducationChanged = this.onEducationChanged.bind(this);
-        this.onSchoolNameChanged = this.onSchoolNameChanged.bind(this);
+    const onEducationChanged = (e) => {
+        setEducation(e.target.value);
     }
 
-    onEducationChanged(e) {
-        this.setState({education: e.target.value})
+    const onSchoolNameChanged = (e) => {
+        setSchoolName(e.target.value);
     }
 
-    onSchoolNameChanged(e) {
-        this.setState({schoolName: e.target.value})
-    }
-
-    render() {
-        const editEnabled = this.props.editEnabled && !this.props.previewEnabled;
-        return (
-            <div className={this.props.className}>
-                <Field 
-                    label="Education"
-                    type="text"
-                    className="education"
-                    editEnabled={editEnabled} 
-                    size={30}                
-                    fieldValue={this.state.education}      
-                    onFieldValueChanged={this.onEducationChanged}  
-                    />
-                    
-                <Field 
-                    label="School Name"
-                    type="text" 
-                    className="school-name"
-                    editEnabled={editEnabled} 
-                    size={35}
-                    fieldValue={this.state.schoolName}        
-                    onFieldValueChanged={this.onSchoolNameChanged}  
-                    />
-                <YearRangeComponent 
-                    editEnabled={editEnabled}
-                    className='education-years'
+    const editEnabled = props.editEnabled && !props.previewEnabled;
+    return (
+        <div className={props.className}>
+            <Field 
+                label="Education"
+                type="text"
+                className="education"
+                editEnabled={editEnabled} 
+                size={30}                
+                fieldValue={education}      
+                onFieldValueChanged={onEducationChanged}  
                 />
-                {(this.props.canRemove && editEnabled) && 
-                    <IconButton
-                        className='remove-education-button icon-button-red'
-                        onClick={(e) => this.props.onRemoveButtonClicked(e, this.props.id)}
-                        text='Remove'
-                        icon={<FaTimesCircle />}
-                    />
-                }
-            </div>
-        )
-    }
+                
+            <Field 
+                label="School Name"
+                type="text" 
+                className="school-name"
+                editEnabled={editEnabled} 
+                size={35}
+                fieldValue={schoolName}        
+                onFieldValueChanged={onSchoolNameChanged}  
+                />
+            <YearRangeComponent 
+                editEnabled={editEnabled}
+                className='education-years'
+            />
+            {(props.canRemove && editEnabled) && 
+                <IconButton
+                    className='remove-education-button icon-button-red'
+                    onClick={(e) => props.onRemoveButtonClicked(e, props.id)}
+                    text='Remove'
+                    icon={<FaTimesCircle />}
+                />
+            }
+        </div>
+    )
 }
 
 export default EducationComponent;
